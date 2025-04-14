d4c15578d10c364f0698639102bb5a79696b5f61
Cedrik Neumann <7921017+m1racoli@users.noreply.github.com>
2025-04-14 00:06:23 -0600

    feat(common): add jwt generator based on jwt-compact
    This one works in no_std too.

--- airflow-common/src/api/auth.rs
+++ airflow-common/src/api/auth.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "jwt-compact")]
+use jwt_compact::AlgorithmExt;
 use serde::Serialize;
 
 use crate::datetime::TimeProvider;
@@ -18,23 +20,26 @@ pub trait JWTGenerator {
     fn generate(&self, method: &str) -> Result<String, Self::Error>;
 }
 
-#[cfg(feature = "jsonwebtoken")]
+#[cfg(any(feature = "jsonwebtoken", feature = "jwt-compact"))]
 #[derive(Debug, Serialize)]
 struct Claims<'i, 'a, 'm> {
     #[serde(rename = "iss")]
     issuer: Option<&'i str>,
     #[serde(rename = "aud")]
     audience: &'a str,
+    #[cfg(feature = "jsonwebtoken")]
     #[serde(rename = "nbf")]
     not_before: i64,
+    #[cfg(feature = "jsonwebtoken")]
     #[serde(rename = "exp")]
     expiry: i64,
+    #[cfg(feature = "jsonwebtoken")]
     #[serde(rename = "iat")]
     issued_at: i64,
     method: &'m str,
 }
 
-#[cfg(feature = "jsonwebtoken")]
+#[cfg(any(feature = "jsonwebtoken", feature = "jwt-compact"))]
 #[derive(Debug, Clone)]
 pub struct DefaultJWTGenerator<T: TimeProvider + Clone> {
     key: SecretString,
@@ -44,7 +49,7 @@ pub struct DefaultJWTGenerator<T: TimeProvider + Clone> {
     issuer: Option<String>,
 }
 
-#[cfg(feature = "jsonwebtoken")]
+#[cfg(any(feature = "jsonwebtoken", feature = "jwt-compact"))]
 impl<T: TimeProvider + Clone> DefaultJWTGenerator<T> {
     pub fn new(key: SecretString, audience: &str, time_provider: T) -> Self {
         DefaultJWTGenerator {
@@ -87,6 +92,33 @@ impl<T: TimeProvider + Clone> JWTGenerator for DefaultJWTGenerator<T> {
     }
 }
 
+#[cfg(feature = "jwt-compact")]
+impl<T: TimeProvider + Clone> JWTGenerator for DefaultJWTGenerator<T> {
+    type Error = jwt_compact::CreationError;
+
+    fn generate(&self, method: &str) -> Result<String, Self::Error> {
+        let now = self.time_provider.now();
+        let leeway = chrono::Duration::seconds(self.valid_for as i64);
+        let time_options = jwt_compact::TimeOptions::new(leeway, move || now.into());
+        let claims = Claims {
+            issuer: self.issuer.as_deref(),
+            audience: &self.audience,
+            // not_before: now,
+            // expiry: now + self.valid_for as i64,
+            // issued_at: now,
+            method,
+        };
+
+        let header = jwt_compact::Header::empty();
+        let claims: jwt_compact::Claims<Claims<'_, '_, '_>> = jwt_compact::Claims::new(claims)
+            .set_not_before(now.into())
+            .set_duration_and_issuance(&time_options, leeway);
+        let bytes: &[u8] = self.key.secret().as_ref();
+        let key = jwt_compact::alg::Hs512Key::new(bytes);
+        jwt_compact::alg::Hs512.token(&header, &claims, &key)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use serde::Deserialize;
@@ -105,8 +137,11 @@ mod tests {
     struct TestClaims {
         iss: Option<String>,
         aud: String,
+        #[cfg(feature = "jsonwebtoken")]
         nbf: i64,
+        #[cfg(feature = "jsonwebtoken")]
         exp: i64,
+        #[cfg(feature = "jsonwebtoken")]
         iat: i64,
         method: String,
     }
@@ -135,6 +170,40 @@ mod tests {
         assert_eq!(claims.nbf, TS);
         assert_eq!(claims.exp, TS + 600);
         assert_eq!(claims.iat, TS);
+        assert_eq!(claims.method, METHOD);
+    }
+
+    #[cfg(feature = "jwt-compact")]
+    #[test]
+    fn test_default_jwt_generator() {
+        let key = SecretString::from(SECRET);
+        let now = DateTime::from_timestamp(TS, 0).unwrap();
+        let time_provider = MockTimeProvider::new(now);
+
+        let generator =
+            DefaultJWTGenerator::new(key.clone(), AUDIENCE, time_provider).with_issuer(ISSUER);
+        let token = generator.generate(METHOD).unwrap();
+
+        let bytes: &[u8] = key.secret().as_ref();
+        let key = jwt_compact::alg::Hs512Key::new(bytes);
+        let token = jwt_compact::UntrustedToken::new(&token).unwrap();
+        let decoded: jwt_compact::Token<TestClaims> = jwt_compact::alg::Hs512
+            .validator(&key)
+            .validate(&token)
+            .unwrap();
+
+        let claims = decoded.claims();
+        let now: chrono::DateTime<chrono::Utc> = now.into();
+        assert_eq!(
+            claims.expiration,
+            Some(now + chrono::Duration::seconds(600))
+        );
+        assert_eq!(claims.not_before, Some(now));
+        assert_eq!(claims.issued_at, Some(now));
+
+        let claims = &claims.custom;
+        assert_eq!(claims.iss, Some(ISSUER.to_string()));
+        assert_eq!(claims.aud, AUDIENCE);
         assert_eq!(claims.method, METHOD);
     }
 }
--- airflow-common/src/api/mod.rs
+++ airflow-common/src/api/mod.rs
@@ -1,4 +1,4 @@
 mod auth;
-#[cfg(feature = "jsonwebtoken")]
+#[cfg(any(feature = "jsonwebtoken", feature = "jwt-compact"))]
 pub use auth::DefaultJWTGenerator;
 pub use auth::JWTGenerator;
