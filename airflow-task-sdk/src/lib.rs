3b695d335c83d72ed2a92e55804d58474a71e989
Cedrik Neumann <7921017+m1racoli@users.noreply.github.com>
2025-08-05 22:15:05 -0600

    refactor(task-sdk): remove prelude

--- airflow-task-sdk/src/lib.rs
+++ airflow-task-sdk/src/lib.rs
@@ -7,13 +7,3 @@ extern crate alloc;
 pub mod api;
 pub mod definitions;
 pub mod execution;
-
-pub mod prelude {
-    pub use crate::api::ExecutionApiClient;
-    pub use crate::api::TaskInstanceApiClient;
-    pub use crate::definitions::Context;
-    pub use crate::definitions::Dag;
-    pub use crate::definitions::DagBag;
-    pub use crate::definitions::Task;
-    pub use crate::definitions::TaskError;
-}
